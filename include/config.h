b94fc99511bc9e532665da4e39cf214cf27fa5e1
QiYuZhuang <andrew_zhuangqy@outlook.com>
2022-09-04 22:17:27 +0800

    fuck encrypt

--- include/config.h
+++ include/config.h
@@ -7,8 +7,8 @@
 #include <memory>
 #include <string>
 
-#define TEST_CONFIG  // test configuration marco switch
-// #define STAT         // statistic
+// #define TEST_CONFIG  // test configuration marco switch
+#define STAT         // statistic
 
 namespace kv {
 #define RDMA_MR_FLAG (IBV_ACCESS_LOCAL_WRITE | IBV_ACCESS_REMOTE_READ | IBV_ACCESS_REMOTE_WRITE)
