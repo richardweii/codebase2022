dd417d2f235d2cf32e3f4f285b7eaa1ead26a0c5
wjp <itwangashao@qq.com>
2022-10-20 17:03:49 +0000

    trty

--- include/config.h
+++ include/config.h
@@ -39,9 +39,9 @@ constexpr int kKeyLength = 16;
 // 一个slab的大小为16字节，这是内存管理的最小单位
 constexpr int kSlabSize = 16;
 // 配置PageSize的大小
-constexpr int kPageSizeBit = 21;  // 20: 1MB
+constexpr int kPageSizeBit = 20;  // 20: 1MB
 constexpr int kPageSize = 1 << kPageSizeBit;
-constexpr int kNumOfKB = 2048;
+constexpr int kNumOfKB = 1024;
 
 // page管理的一个slot的大小最小为5个slab，也就是80字节
 constexpr int kSlabSizeMin = 5;  // 5 * 16 = 80 Bytes
@@ -80,7 +80,7 @@ constexpr int MAX_BATCH_NUM = 256;
 constexpr int MAX_CQE = MAX_BATCH_NUM;
 constexpr int MAX_QP_WR = MAX_BATCH_NUM;
 constexpr int MAX_CQ = MAX_BATCH_NUM;
-constexpr int MAX_INITIATOR_DEPTH = 64;
+constexpr int MAX_INITIATOR_DEPTH = 128;
 
 using Addr = int32_t;
 constexpr Addr INVALID_ADDR = (-1);
